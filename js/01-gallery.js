7db6dceddde69a2725a564d2ec699f8de583a472
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2022-05-25 10:12:04 +0300

    виконує таск 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,51 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('div.gallery');
+
+
+const galleryItem = galleryItems.
+    map(({ description, original, preview }) => {
+        return `<div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                        />
+                    </a>
+                </div>`
+    }).join('');
+
+galleryEl.innerHTML = galleryItem;
+
+
+galleryEl.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    const imgOriginal = e.target.dataset.source;
+    if (!imgOriginal) {
+        return;
+    }
+
+    const instance = basicLightbox.create(`<img src="${imgOriginal}">`)
+    instance.show()
+});
+
+
+// Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, і вказуватися в href посилання. Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
+
+// <div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div>
+// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. Заборони цю поведінку за замовчуванням.
+
